932a9f884d5e1450de2fefd0f81b23c0af482cf6
AbubakarShf <ItsAbubakarShf@gmail.com>
2021-05-06 00:50:21 +0500

    Navbar-error

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,77 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 const Navbar = () => {
+    const [showLinks, setShowLinks] = useState(true);
     return (
-        <Container>
-            <NavoBar>
-                <WebLogo>
-                    <h3>Logo</h3>
-                </WebLogo>
-                <WebLinks>
-                    <ul>
-                        <li>
-                            <a href="http://facebook.com" target="_blank">
-                                Link1
-                            </a>
-                        </li>
-                        <li>
-                            <a href="http://facebook.com" target="_blank">
-                                Link2
-                            </a>
-                        </li>
-                        <li>
-                            <a href="http://facebook.com" target="_blank">
-                                Link3
-                            </a>
-                        </li>
-                        <li className="last">
-                            <a href="http://facebook.com" target="_blank">
-                                Link4
-                            </a>
-                        </li>
-                    </ul>
-                </WebLinks>
-            </NavoBar>
-        </Container>
+        <Nav>
+            <LeftSide>
+                <h2>Logo</h2>
+            </LeftSide>
+            <RightSide>
+                <Links>
+                    <div id={showLinks ? "hidden" : ""}>
+                        <ul>
+                            <li>
+                                <a href="https://www.facebook.com">Home</a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com">About</a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com">ContactUs</a>
+                            </li>
+                            <li>
+                                <a href="https://www.facebook.com">Details</a>
+                            </li>
+                        </ul>
+                    </div>
+                </Links>
+            </RightSide>
+        </Nav>
     );
 };
-
 export default Navbar;
-const Container = styled.div`
+const Nav = styled.div`
+    font-family: Arial, Helvetica, sans-serif;
+    display: flex;
     background-color: coral;
     color: white;
-    font-size: 24px;
-    font-weight: 700;
+    font-size: 20px;
+    font-weight: 500;
     width: 100vw;
-    height: auto;
+    height: 80px;
 `;
-const NavoBar = styled.nav`
+const LeftSide = styled.div`
+    flex: 30%;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding-left: 70px;
+    @media only screen and (max-width: 701px) {
+        font-size: 17px;
+    }
+`;
+const RightSide = styled.div`
+    flex: 70%;
     display: flex;
     justify-content: flex-end;
-`;
-const WebLogo = styled.div`
-    padding: 22px 22px;
     align-items: center;
-    margin-right: auto;
-    margin-left: 20px;
 `;
-const WebLinks = styled.div`
+const Links = styled.div`
+    max-height: 80px;
     ul {
-        align-items: center;
-        padding: 22px 22px;
-        align-items: center;
-        .last {
-            margin-right: 20px;
-        }
+        margin-right: 70px;
         li {
             list-style: none;
             display: inline-block;
             a {
                 text-decoration: none;
-                padding-left: 50px;
-                padding-right: 20px;
                 color: white;
             }
         }
+        li a {
+            margin-right: 20px;
+            margin-left: 20px;
+        }
+    }
+    @media only screen and (max-width: 701px) {
+        font-size: 17px;
+    }
+    #hidden {
+        position: absolute;
+        right: 0;
+        top: 80px;
+        height: auto;
+        width: 100vw;
+        background-color: #000000;
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-end;
+        align-items: center;
+    }
+
+    @media only screen and (max-width: 550px) {
+        display: none;
     }
 `;
